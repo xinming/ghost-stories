4bc94e79717d5c300f8fc5846d82a03022f12eda
xinming <zhaoxmmusic@msn.com>
2011-10-31 22:11:17 +0700

    added db

--- db/migrate/20111031111205_create_stories.rb
+++ db/migrate/20111031111205_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
       t.string :name
-      t.string :source
+      t.text :source
       t.text :story
       t.string :writer
       t.integer :genre_id
